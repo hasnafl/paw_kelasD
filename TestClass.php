1c82fcb261cba92ecbfd693119b1d1227e3db177
hasnafl <hasnalutfiyah@gmail.com>
2017-12-04 23:56:20 +0700

    ini php unit

--- TestClass.php
+++ TestClass.php
@@ -4,11 +4,15 @@ class TestClass extends PHPUnit_Framework_TestCase
 {
         public function testFile()
         {
-                ob_start();
-                include 'umum.php';
-                $content = ob_get_contents();
-                ob_end_clean();
-                $this->assertEquals('11, 12-1', $content);
+                include 'file.php';
+                $content = $user;
+                $this->assertEquals(1, $content);
+        }
+        public function testAkses()
+        {
+        	include 'file.php';
+        	$content = $hakakses;
+        	$this->assertEquals('klinik1', $content);
         }
 }
 
