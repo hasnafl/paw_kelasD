a56a32b98515d7d5574fca776c14ab884085f6ea
hasnafl <hasnalutfiyah@gmail.com>
2018-01-08 22:28:35 +0700

    oke aku lelah

--- test/TestClass.php
+++ test/TestClass.php
@@ -4,13 +4,13 @@ class TestClass extends PHPUnit_Framework_TestCase
 {
         public function testFile()
         {
-                include '..\file.php';
+                include 'file.php';
                 $content = $user;
                 $this->assertEquals(1, $content);
         }
         public function testAkses()
         {
-        	include '..\file.php';
+        	include 'file.php';
         	$content = $hakakses;
         	$this->assertEquals('klinik1', $content);
         }
